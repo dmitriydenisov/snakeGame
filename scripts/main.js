d192d91d88482da07de8b3231126f08c7cdf073c
dmitriydenisov <dmitriyd2009@yandex.ru>
2020-03-22 12:32:37 +0400

    добавили тело к змейке

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,17 @@
-const ROWS = 10 
-const COLUMNS = 10 
-const START_COOLDOWN = 250 
-const LEVEL_COOLDOWN = 5
+const ROWS = 10
+const COLUMNS = 10
+const COOLDOWN = 250
 
-const CELL_SIZE = 50 
-const CELL_MARGIN = 2 
-const GAME_PADDING = 5 
+const CELL_SIZE = 50
+const CELL_MARGIN = 2
+const GAME_PADDING = 5
 
-const FOOD_COLOR = 'green' 
-const SNAKE_COLOR =  'gray' 
-const FREE_COLOR = 'rgb(240, 240, 240)' 
+const FOOD_COLOR = 'green'
+const SNAKE_COLOR =  'gray'
+const FREE_COLOR = 'rgb(240, 240, 240)'
 
-const canvas  = document.querySelector('canvas') 
-const context = canvas.getContext('2d') 
+const canvas  = document.querySelector('canvas')
+const context = canvas.getContext('2d')
 
 canvas.width = CELL_SIZE * COLUMNS + (COLUMNS - 1) * CELL_MARGIN + 2 * GAME_PADDING
 canvas.height = CELL_SIZE * ROWS + (ROWS - 1) * CELL_MARGIN + 2 * GAME_PADDING
@@ -21,60 +20,26 @@ const map = creatGameMap(COLUMNS, ROWS)
 
 getRandomFreeCell(map).food = true
 
-const cell = getRandomFreeCell(map)
-const snake = [cell, cell, cell, cell, cell, cell, cell, cell, cell, cell, cell]
-
-cell.snake = true
-
- 
+const snake = [getRandomFreeCell(map)] 
 snake[0].snake = true
 
-let snakeDirect = 'top'
-let nextSnakeDirect = 'top'
+let snakeDirect = ''
 
 
-//обновление изменения изображения при обновлении экрана
+//регистрация функции к следующему моменту обновлению монитоара
 requestAnimationFrame(loop)
 
 let prevTick = 0
-let play = true
-let cooldown = START_COOLDOWN
 
 function loop (timestamp){
-    //зацикливание обновления
+    //зацикливание вызова функции
     requestAnimationFrame(loop)
     
     clearCanvas()
 
-    if (prevTick + cooldown <= timestamp && play) {
+    if (prevTick + COOLDOWN <= timestamp) {
+        moveSnake() 
         prevTick = timestamp
-
-        snakeDirect = nextSnakeDirect
-        moveSnake()
-        const head = snake[0]
-        const tails = snake[snake.length - 1]
-        
-        if ( head.food){
-            head.food = false
-            snake.push(tails)
-
-            getRandomFreeCell(map).food = true
-            cooldown -= LEVEL_COOLDOWN
-        }
-
-        //проверка пересечения головы и тела змейки
-
-        let isEnd = false
-        for ( let i = 1; i < snake.length; i++ ) {
-            if ( snake[i] === snake[0] ) {
-                isEnd = true
-                break
-            }
-        }
-        if (isEnd) {
-            alert("Конец игры!")
-            play = false
-        }
     }
 
   
@@ -83,21 +48,14 @@ function loop (timestamp){
 
 document.addEventListener("keydown", function ( event ) {
     if ( event.key === "ArrowUp" ) {
-        if ( snake.length === 1 || snakeDirect === "left" || snakeDirect === "right" ){
-            nextSnakeDirect = 'top' 
-        }
-           } else if ( event.key === "ArrowDown" ) {
-        if (snake.length === 1 || snakeDirect === "left" || snakeDirect === "right" ){
-            nextSnakeDirect = 'down'
-        }
-            } else if ( event.key === "ArrowLeft" ) {
-        if (snake.length === 1 || snakeDirect === "top" || snakeDirect === "down" ){
-            nextSnakeDirect = 'left'
-        }
-            } else if ( event.key === "ArrowRight" ) {
-        if (snake.length === 1 || snakeDirect === "top" || snakeDirect === "down" ){
-            nextSnakeDirect = 'right'
-        }
-        
+        snakeDirect = 'top'
+    } else if ( event.key === "ArrowDown" ) {
+        snakeDirect = 'down'
+    } else if ( event.key === "ArrowLeft" ) {
+        snakeDirect = 'left'
+    } else if ( event.key === "ArrowUp" ) {
+        snakeDirect = 'top'
+    } else if ( event.key === "ArrowRight" ) {
+        snakeDirect = 'right'
     }
-}) 
+})
